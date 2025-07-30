0842d585cdccbc1f99d7bf6231addd138c08bc99
Nem1x <nemanjanedeljkovic738@gmail.com>
2025-07-30 14:03:04 +0000

    sea: Ship cgroup and task_profiles 30

--- device.mk
+++ device.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
 
 # Control groups/Task profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # ConsumerIR
 PRODUCT_PACKAGES += \
