0957e9c6439a94d4adf56177377255d68744dfde
Nem1x <nemanjanedeljkovic738@gmail.com>
2025-07-30 14:00:05 +0000

    sea: extract-files: Fixup blob patches

--- extract-files.py
+++ extract-files.py
@@ -77,7 +77,10 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .add_needed('liblog.so'),
 
-    'vendor/lib64/libneuron_adapter_mgvi.so': blob_fixup()
+    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so', 'vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+
+    ( 'vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_createFromHandle')
         .clear_symbol_version('AHardwareBuffer_describe')
@@ -86,18 +89,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
 
-    'vendor/lib64/libneuralnetworks_sl_driver_mtk_legacy_prebuilt.so': blob_fixup()
-        .clear_symbol_version('AHardwareBuffer_allocate')
-        .clear_symbol_version('AHardwareBuffer_createFromHandle')
-        .clear_symbol_version('AHardwareBuffer_describe')
-        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
-        .clear_symbol_version('AHardwareBuffer_lock')
-        .clear_symbol_version('AHardwareBuffer_release')
-        .clear_symbol_version('AHardwareBuffer_unlock')
-        .add_needed('libbase_shim.so'),
-
-    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
-        .add_needed('libbase_shim.so'),
 }
 
 module = ExtractUtilsModule(
