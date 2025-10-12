e9390f7973dc2c75f4a1700ae7d41e43d522fcb2
Lesano Tsuki <leandronogueira@sapo.pt>
2025-10-12 18:52:16 +0100

    Fix cloning conditions in vendorsetup.sh for sea device and dependencies

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,23 +4,22 @@ if [ ! -d "hardware/mediatek" ]; then
   git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 fi
 if [ ! -d "vendor/xiaomi/sea" ]; then
-  git clone https://github.com/lesano/android_device_xiaomi_sea device/xiaomi/sea
-fi
-if [ ! -d "device/xiaomi/sea-prebuilt"]; then
-  git clone https://github.com/LiamSDK/android_device_xiaomi_sea-prebuilt device/xiaomi/sea-prebuilt
+  git clone https://github.com/lesano/android_device_xiaomi_sea -b aosp-a15 device/xiaomi/sea
 fi
 if [ ! -d "vendor/xiaomi/sea"]; then
   git clone https://github.com/lesano/android_vendor_xiaomi_sea vendor/xiaomi/sea
 fi
+if [ ! -d "device/xiaomi/sea-prebuilt"]; then
+  git clone https://github.com/LiamSDK/android_device_xiaomi_sea-prebuilt device/xiaomi/sea-prebuilt
+fi
 if [ ! -d "device/xiaomi/mt6871" ]; then
   git clone https://github.com/mt6781-devs/android_kernel_xiaomi_mt6781 kernel/xiaomi/mt6781
 fi
-if [ -d "vendor/mediatek/ims"]; then
+if [ ! -d "vendor/mediatek/ims"]; then
   git clone https://github.com/mt6781-devs/android_vendor_mediatek_ims vendor/mediatek/ims
 fi
-if [ -d "device/mediatek/sepolicy_vndr"]; then
+if [ ! -d "device/mediatek/sepolicy_vndr"]; then
   git clone https://github.com/mt6781-devs/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 
-if [ -d "hardware/lineage/interfaces/sensor"]; then
+if [ ! -d "hardware/lineage/interfaces/sensor"]; then
   rm hardware/lineage/interfaces/sensor
-fi
