8a30bdf2522b190574909f05adb29b94a1ca2757
Lesano Tsuki <leandronogueira@sapo.pt>
2025-10-12 18:49:53 +0100

    Add cloning for sea-prebuilt device directory in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,9 @@ fi
 if [ ! -d "vendor/xiaomi/sea"]; then
   git clone https://github.com/lesano/android_vendor_xiaomi_sea vendor/xiaomi/sea
 fi
+if [ ! -d "device/xiaomi/sea-prebuilt"]; then
+  git clone https://github.com/LiamSDK/android_device_xiaomi_sea-prebuilt device/xiaomi/sea-prebuilt
+fi
 if [ ! -d "device/xiaomi/mt6871" ]; then
   git clone https://github.com/mt6781-devs/android_kernel_xiaomi_mt6781 kernel/xiaomi/mt6781
 fi
